46e0e9d3759298ea519d12516f7caf4c28d154df
akshaypatel <akshaypatel@akshays-MacBook-Pro.local>
2017-12-07 16:18:09 -0600

    fixing things in javascript css and html pages

--- js/myjavascriptfile.js
+++ js/myjavascriptfile.js
@@ -42,5 +42,16 @@ function validateForm() {
     	alert("Thanks! I will contact you soon");
     }
 }
+
+jQuery(document).ready(function($) {
+  $('#style_it').on('click', function(){
+    $('#styled_video')[0].play();
+    $('#styled_video_container').addClass('style_it');
+  });
+  $('#stop_style_it').on('click', function(){
+    $('#styled_video_container').removeClass('style_it');
+    $('#styled_video')[0].pause();
+  });
+});
     
 
